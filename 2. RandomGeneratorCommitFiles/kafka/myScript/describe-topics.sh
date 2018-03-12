88dd655801d8738f52aab92903c7c66336a67673
hayari <noureddine.hayari@hotmail.com>
2018-03-12 18:04:16 +0100

    supperssion à cause du nom du rep

--- 2. RandomGeneratorCommitFiles/kafka/myScript/describe-topics.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#cd ~/DataProject
-cd ./
-
-# List existing topics
-kafka/bin/kafka-topics.sh --describe \
-    --topic topiccommit \
-    --zookeeper localhost:2181
--- 2. RandomGeneratorCommitFiles/kafka/myScript/run-zookeeper.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#cd ~/DataProject
-cd ./
-
-kafka/bin/zookeeper-server-start.sh kafka/config/zookeeper.properties
